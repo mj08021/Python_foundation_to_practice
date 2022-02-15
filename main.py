b4d80b8493617e487193ac7b5c9aa051e54d5369
unaire L <69451980+mj08021@users.noreply.github.com>
2022-02-15 15:52:20 +0000

    2022/02/16

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-#language = "python3"
-run = "python turtle_graphic.py"
-
-
-
-# 깃허브 연동방법
-# git add .
-# git commit -m "commit!!"
-# git push origin master #
-# git push origin main
-
-#강제 덮어씌우기
-# git push -f origin
+exec(open("Part 1/05_while.py").read())
--- turtle_graphic.py
+++
@@ -1,8 +0,0 @@
-
-## 파이썬 터틀 모듈을 가져오고 있다.
-##import turtle
-
-## = turtle.Pen()
-
-
-print("HELLO")
