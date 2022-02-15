eaefb2bc55d46cf7bca61161e5eb89a910e94bfb
unaireL <69451980+mj08021@users.noreply.github.com>
2022-02-15 14:17:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#language = "python3"
+run = "python turtle_graphic.py"
+
+
+
+# 깃허브 연동방법
+# git add .
+# git commit -m "commit!!"
+# git push origin master #
+# git push origin main
+
+#강제 덮어씌우기
+# git push -f origin
---
+++ turtle_graphic.py
@@ -0,0 +1,8 @@
+
+## 파이썬 터틀 모듈을 가져오고 있다.
+##import turtle
+
+## = turtle.Pen()
+
+
+print("HELLO")
